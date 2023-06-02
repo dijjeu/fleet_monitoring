e5a562c96d0caa5a4a1c92a3b728d2461c30c856
dijjeu <patriciadiez0505@gmail.com>
2023-06-02 17:24:06 +0800

    Update dashboard_screen.dart

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:fleet_monitoring/notification.dart';
 import 'package:fleet_monitoring/vehicle/vehicle.dart';
+import 'package:fleet_monitoring/vehicle/vehicle_entry.dart';
 import 'package:fleet_monitoring/vehicle_input.dart';
 import 'package:fleet_monitoring/vehicle_service.dart';
 import 'package:flutter/material.dart';
@@ -24,6 +25,14 @@ class _DashboardScreenState extends State<DashboardScreen> {
   int pageNum = 0;
   List<VehicleDetails> vehicleDetails = List.empty(growable: true);
 
+  /// VEHICLE DETAILS DISPLAY
+  TextEditingController carMakeController = TextEditingController();
+  TextEditingController yearModelController = TextEditingController();
+  TextEditingController plateNumController = TextEditingController();
+
+  int selectedIndex = -1;
+
+
   @override
   void initState() {
     super.initState();
@@ -42,7 +51,32 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   void dispose() {
     _pageController.dispose();
+    carMakeController.dispose();
+    yearModelController.dispose();
+    plateNumController.dispose();
     super.dispose();
+  }
+
+  void addVehicle() {
+    String carMake = carMakeController.text.trim();
+    String yearModel = yearModelController.text.trim();
+    String plateNum = plateNumController.text.trim();
+
+    if (carMake.isNotEmpty && yearModel.isNotEmpty && plateNum.isNotEmpty) {
+      setState(() {
+        vehicleDetails.add(VehicleDetails(
+          carMake: carMake,
+          yearModel: yearModel,
+          plateNum: plateNum,
+          color: '', regisNum: '', orNum: '', regisDate: '', orDateIssued: '',
+        ));
+      });
+
+      carMakeController.text = '';
+      yearModelController.text = '';
+      plateNumController.text = '';
+      _pageController.animateToPage(vehicleDetails.length - 1, duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+    }
   }
 
   void onDidReceiveNotificationResponse(
@@ -91,152 +125,161 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   color: Colors.black87,
                 ),
               ),
-              searchBar(),
-              /*vehicleDetails.isEmpty
-                  ? Text(
-                'No vehicles listed yet..',
-                style: GoogleFonts.poppins(
-                  fontSize: 22,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.black54,
-                ),
-              ) :*/
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () {},
+                child: Text('View all vehicles'),
+              ),
+              const SizedBox(height: 20),
               SizedBox(
                 height: 300,
                 child: PageView.builder(
                   controller: _pageController,
-                    onPageChanged: (index) {
+                  onPageChanged: (index) {
+                    setState(() {
                       pageNum = index;
-                    },
-                    itemBuilder: (_, index) {
-                      return AnimatedBuilder(
-                        animation: _pageController,
-                        builder: (ctx, child) {
-                          return child!;
-                        },
-                        child: Container(
-                          child: Image.asset('assets/license.png'),
-                          margin: const EdgeInsets.only(
-                            right: 4,
-                            left: 4,
-                            top: 36,
-                            bottom: 12,
-                          ),
-                          height: 400,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(24),
-                            color: Colors.blue[800],
-                          ),
+                    });
+                  },
+                  itemBuilder: (_, index) {
+                    return AnimatedBuilder(
+                      animation: _pageController,
+                      builder: (ctx, child) {
+                        return child!;
+                      },
+                      child: Container(
+                        child: Image.asset('assets/license.png'),
+                        margin: const EdgeInsets.only(
+                          right: 4,
+                          left: 4,
+                          top: 36,
+                          bottom: 12,
                         ),
-                      );
-                    },
-                  itemCount: 5,
+                        height: 400,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: vehicleDetails.length,
                 ),
               ),
               const SizedBox(height: 2),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(5, (index) =>
-                  Container(
+                children: List.generate(vehicleDetails.length, (index) {
+                  return Container(
                     margin: const EdgeInsets.all(2),
                     child: Icon(
                       Icons.circle,
                       size: 8,
                       color: pageNum == index ? Colors.blue[800] : Colors.grey,
                     ),
-                  ),
-                ),
+                  );
+                }),
               ),
-              Padding(
+              const SizedBox(height: 10),
+              if (vehicleDetails.isNotEmpty)
+                Padding(
                   padding: const EdgeInsets.all(20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'CAR 9310', //plate number
-                      style: GoogleFonts.poppins(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.black87,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        vehicleDetails[pageNum].plateNum,
+                        style: GoogleFonts.poppins(
+                          fontSize: 35,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.black87,
+                        ),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          'Mazda 2', //car make
-                          style: GoogleFonts.poppins(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black87,
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Text(
-                          '2019', //year model
-                          style: GoogleFonts.poppins(
-                            fontSize: 25,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black87,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        TextButton(
-                          onPressed: () {},
-                          child: Row(
-                            children: [
-                              Icon(Icons.info_outline_rounded, color: Colors.black87),
-                              SizedBox(width: 8), // Optional spacing between the icon and text
-                              Text('Information'),
-                            ],
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: Row(
-                            children: [
-                              Icon(Icons.edit_note_outlined, color: Colors.black87),
-                              SizedBox(width: 8), // Optional spacing between the icon and text
-                              Text('Edit'),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Row(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          Text('Add a new vehicle'),
-                          SizedBox(width: 50),
-                          Icon(
-                            Icons.add,
-                            color: Colors.white,
+                          Text(
+                            vehicleDetails[pageNum].carMake,
+                            style: GoogleFonts.poppins(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            vehicleDetails[pageNum].yearModel,
+                            style: GoogleFonts.poppins(
+                              fontSize: 25,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black87,
+                            ),
                           ),
                         ],
                       ),
-                      style: ButtonStyle(
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => VehicleEntry(vehicle: vehicleDetails[5])),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.info_outline_rounded, color: Colors.black87),
+                                SizedBox(width: 8),
+                                Text('Information'),
+                              ],
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Row(
+                              children: [
+                                Icon(Icons.edit_note_outlined, color: Colors.black87),
+                                SizedBox(width: 8),
+                                Text('Edit'),
+                              ],
+                            ),
+                          )
 
-                        shadowColor: MaterialStateProperty.all<Color>(Colors.black),
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue.shade800),
+                        ],
                       ),
-                    ),
-                    TextButton(
-                        onPressed: () {},
-                        child: Text('View all vehicles'),
-                    ),
-                  ],
+                      ElevatedButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                content: vehicleInput(),
+                              );
+                            },
+                          );
+                        },
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text('Add a new vehicle'),
+                            const SizedBox(width: 50),
+                            Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                        style: ButtonStyle(
+                          shadowColor: MaterialStateProperty.all<Color>(Colors.black),
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue.shade800),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
             ],
           ),
         ),
@@ -261,4 +304,66 @@ class _DashboardScreenState extends State<DashboardScreen> {
       ),
     );
   }
+
+  Widget vehicleInput() {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const SizedBox(height: 50),
+              Text(
+                'Vehicle Details',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[800],
+                ),
+              ),
+              TextField(
+                controller: carMakeController,
+                decoration: const InputDecoration(labelText: 'Car Make'),
+              ),
+              TextField(
+                controller: yearModelController,
+                decoration: const InputDecoration(labelText: 'Year Model'),
+              ),
+              TextField(
+                controller: plateNumController,
+                decoration: const InputDecoration(labelText: 'Plate Number'),
+              ),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    child: Text(
+                      'Done',
+                      style: TextStyle(
+                        color: Colors.red[400],
+                      ),
+                    ),
+                    onPressed: addVehicle,
+                  ),
+                  TextButton(
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
 }
